b2369a960efd6323596271ea1125134fd149edf7
6awdsasdaw9 <69734315+6awdsasdaw9@users.noreply.github.com>
2022-10-27 17:51:25 +0300

    Hero Move and Camera Follow

--- Assets/Script/Hero/HeroMovement.cs
+++ Assets/Script/Hero/HeroMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using Script.CameraLogic;
 using Script.Infrastructure;
 using Script.Services.Input;
 using UnityEngine;
@@ -7,12 +8,12 @@ using UnityEngine.Serialization;
 namespace Script.Hero
 {
     [RequireComponent(typeof(Rigidbody2D))]
-    internal class HeroMovement: MonoBehaviour
-    { 
-        [SerializeField] private  float _speed;
+    internal class HeroMovement : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
         [SerializeField] private Rigidbody2D _rb;
         private RotationController rotate;
-        public  Vector2 velocity => _rb.velocity;
+        public Vector2 velocity => _rb.velocity;
         private InputService _inputService;
         private Camera _camera;
 
@@ -25,14 +26,16 @@ namespace Script.Hero
         private void Start()
         {
             _camera = Camera.main;
+            CameraFollow();
         }
+
 
         public void FixedUpdate()
         {
-            _rb.velocity =  new Vector2(_inputService.horizontalAxis, _rb.velocity.y) * _speed* Time.deltaTime;
+            _rb.velocity = new Vector2(_inputService.horizontalAxis, _rb.velocity.y) * _speed * Time.deltaTime;
             rotate.Turn(_inputService.horizontalAxis);
         }
-        
-        
+
+        private void CameraFollow() => _camera.GetComponent<CameraFollow>().Follow(gameObject);
     }
 }
