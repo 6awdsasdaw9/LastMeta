cb6fc612b584dd1dda18817559f350191fc47831
6awdsasdaw9 <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2023-04-23 20:10:04 +0300

    Audio Event Instance

---
+++ Assets/Code/Audio/AudioEventInstance.cs
@@ -0,0 +1,35 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
+
+namespace Code.Audio
+{
+    public class AudioEventInstance : MonoBehaviour
+    {
+        [SerializeField] private EventReference _event;
+        private EventInstance _instance;
+        
+        
+        private void OnEnable()
+        {
+            PlayEvent();
+        }
+
+        private void OnDisable()
+        {
+            _instance.stop(STOP_MODE.ALLOWFADEOUT);
+        }
+
+        
+        private void PlayEvent()
+        {
+            if(_event.IsNull)
+                return;
+            
+            _instance = RuntimeManager.CreateInstance(_event);
+            _instance.start();
+        }
+
+    }
+}
