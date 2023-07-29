8ebbce5ce18b8824ee690bb57726fdc9d9bb18ce
6awdsasdaw9 <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2023-07-29 15:39:56 +0300

    _

--- Assets/Code/Character/Hero/Abilities/Ability.cs
+++ Assets/Code/Character/Hero/Abilities/Ability.cs
@@ -5,8 +5,7 @@ namespace Code.Character.Hero.Abilities
         protected HeroAbilityType Type { get; set; }
         protected bool IsOpen => Level >= 0;
         public int Level { get; protected set; }
-        
-        
+
         public float CooldownTime { get; protected set; }
         public abstract void StartApplying();
         public abstract void StopApplying();
